c1046d565d841c73bddc48d508032ae745cce93e
AZ <speedsterk@hotmail.com>
2024-02-24 12:04:41 +0200

    Refactor code to improve performance and readability

---
+++ 3ai.solutions.Saferpay.Core/Models/PaymentPageInitializeRQ.cs
@@ -0,0 +1,40 @@
+namespace _3ai.solutions.Saferpay.Models
+{
+    public class PaymentPageInitializeRQ
+    {
+
+    }
+
+    public enum PaymentMethods
+    {
+        ACCOUNTTOACCOUNT,
+        ALIPAY,
+        AMEX,
+        BANCONTACT,
+        BONUS,
+        DINERS,
+        CARD,
+        DIRECTDEBIT,
+        EPRZELEWY,
+        EPS,
+        GIROPAY,
+        IDEAL,
+        INVOICE,
+        JCB,
+        KLARNA,
+        MAESTRO,
+        MASTERCARD,
+        MYONE,
+        PAYCONIQ,
+        PAYDIREKT,
+        PAYPAL,
+        POSTCARD,
+        POSTFINANCE,
+        POSTFINANCEPAY,
+        SOFORT,
+        TWINT,
+        UNIONPAY,
+        VISA,
+        WLCRYPTOPAYMENTS,
+    }
+}
