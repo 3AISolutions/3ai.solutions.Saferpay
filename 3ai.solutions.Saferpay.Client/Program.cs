f9094b0b8ac1bc579d4a5c9af041be27d6e97ca2
AZ <speedsterk@hotmail.com>
2024-02-28 18:48:22 +0200

    .

--- 3ai.solutions.Saferpay.Client/Program.cs
+++ 3ai.solutions.Saferpay.Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using _3ai.solutions.Saferpay.Client.Data;
+using _3ai.solutions.Saferpay;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddSingleton<SaferpayService>(new SaferpayService(new SaferpayConfig
+{
+    BaseUrl = builder.Configuration["Saferpay:BaseUrl"],
+    Username = builder.Configuration["Saferpay:Username"],
+    Password = builder.Configuration["Saferpay:Password"]
+}));
 
 var app = builder.Build();
 
