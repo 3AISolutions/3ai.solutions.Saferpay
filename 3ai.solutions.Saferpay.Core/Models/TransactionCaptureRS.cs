5fd9724b4c701e6ee9e4f8abd74bffa2effcb28b
Andreas Michaelou <andreas@belugga.io>
2024-03-01 10:20:03 +0200

    .

--- 3ai.solutions.Saferpay.Core/Models/TransactionCaptureRS.cs
+++ 3ai.solutions.Saferpay.Core/Models/TransactionCaptureRS.cs
@@ -3,7 +3,7 @@ namespace _3ai.solutions.Saferpay.Models
     public class TransactionCaptureRS : BaseResponse
     {
         public string CaptureId { get; set; }
-        public string Status { get; set; }
+        public TransactionStatus Status { get; set; }
         public string Date { get; set; }
         public object Invoice { get; set; }
     }
