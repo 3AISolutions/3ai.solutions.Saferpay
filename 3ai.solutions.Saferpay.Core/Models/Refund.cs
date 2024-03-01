53c240d12c9bb014e73f155c3a4296fd5fd12515
AZ <speedsterk@hotmail.com>
2024-03-01 10:11:59 +0200

    Add Description and RestrictRefundAmountToCapturedAmount properties to Refund model

--- 3ai.solutions.Saferpay.Core/Models/Refund.cs
+++ 3ai.solutions.Saferpay.Core/Models/Refund.cs
@@ -4,5 +4,7 @@ namespace _3ai.solutions.Saferpay.Models
     {
         public Amount Amount { get; set; }
         public string OrderId { get; set; }
+        public string Description { get; set; }
+        public bool RestrictRefundAmountToCapturedAmount { get; set; }
     }
 }
